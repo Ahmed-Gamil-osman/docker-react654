96fec9d9af83c15ec4a9136ebe7be349851cb675
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2026-03-07 21:50:47 +0200

    add the new test cases

--- src/App.test.js
+++ src/App.test.js
@@ -47,23 +47,6 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 
